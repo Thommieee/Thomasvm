7642560bc78a3ae6cffd742a5becb170ca431990
Thommieee <60047417+Thommieee@users.noreply.github.com>
2020-02-11 20:09:05 +0100

    update

---
+++ commands/food.js
@@ -0,0 +1,28 @@
+const randomPuppy = require('random-puppy');
+
+module.exports.run = async (bot, message, args) => {
+
+    let reddit = [
+        "food",
+        "FoodPorn"
+    ]
+
+    let subreddit = reddit[Math.floor(Math.random() * reddit.length)];
+
+    message.channel.startTyping();
+
+    randomPuppy(subreddit).then(async url => {
+            await message.channel.send({
+                files: [{
+                    attachment: url,
+                    name: 'food.png'
+                }]
+            }).then(() => message.channel.stopTyping());
+    }).catch(err => console.error(err));
+
+};
+
+module.exports.help = {
+    name: 'food',
+    description: "Laat een random food plaatje zien van r/food en r/foodporn"
+}
--- commands/hallo.js
+++ commands/hallo.js
@@ -1,6 +1,6 @@
 const discord = require("discord.js");
 
-module.exports.run = async(bot, message, args) => {
+module.exports.run = async (bot, message, args) => {
 
     return message.channel.send("Hallo");
 
